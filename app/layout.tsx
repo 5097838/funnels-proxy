9ed2fb7154b1cc38764341196a618cb56db2a8f2
5097838 <5097838@gmail.com>
2026-03-30 21:52:53 +0300

    Deploy funnels-proxy app

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: "Landing Page" };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (<html lang="ru"><body>{children}</body></html>);
+}
